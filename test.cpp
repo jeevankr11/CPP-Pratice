9ddeed4c518c413b0cb6c121edc91b3e8da800b5
Jeevankr11 <jk678580@gmail.com>
2026-06-15 22:06:04 +0530

    First commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a=4;
+    
+    for (int i=0; i<a; i++) {
+        
+        for (int j=0; j<a*2+1; j++) {
+            cout << "  ";
+            
+        }
+        cout << "%" ;
+
+
+        cout <<endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
