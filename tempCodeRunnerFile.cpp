85dca801394e0d59910b2a41bc1ba921049e548b
Jeevankr11 <jk678580@gmail.com>
2026-06-21 17:44:21 +0530

    Added binary pattern and updated programs

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,2 @@
-(a != 0) {
-    //     for (int b=0; b<2*n-2*a-3; b++) {
-    //         cout << "  ";
-
-    //     }
-
-    //     cout << "*";
-        
-    //     cout << endl;
-    // }
+for (int b=0; b<2*(n-a)-5; b++) {
+            cout << " ";
--- test.cpp
+++ test.cpp
@@ -3,20 +3,8 @@ using namespace std;
 
 
 int main() {
-    int a=4;
     
-    for (int i=0; i<a; i++) {
-        
-        for (int j=0; j<a*2+1; j++) {
-            cout << "  ";
-            
-        }
-        cout << "%" ;
-
-
-        cout <<endl;
-    }
-
+    cout << sizeof(3) << endl;
 
 
 
